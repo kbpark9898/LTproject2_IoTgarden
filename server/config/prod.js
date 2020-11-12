4a4c473ccc6b3c5c8ff1782a8b085ee0be8a7fc5
kbpark9898 <kbpark989@gmail.com>
2020-11-13 02:54:11 +0900

    delete useless file

--- server/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    mongoURI: "YOUR mongoDB URI"
-}
-// if you use heroku, you can use environment variable process.env.MONGO_URI"
--- server/index.js
+++ server/index.js
@@ -1,90 +1,111 @@
 const express = require('express')
 const app = express()
 const port = 6000
-const {User} = require('../models/user')
-const {rpi} = require('../models/rpi')
+const cryptoM=require('./config/crypto')
 const bodyParser = require('body-parser')
-const mongoose = require("mongoose")
-const key = require('./config/key')
-const user = require('../models/user')
+const jwt = require('./modules/token')
 const cookieParser = require('cookie-parser')
 const {auth} = require('./middleware/auth')
-
+const mysql = require('mysql')
+const session = require('express-session')
+const {auth} = require('./middleware/auth_sql')
+const { default: Axios } = require('axios')
 app.use(bodyParser.urlencoded({extended:'true'}))
 app.use(bodyParser.json())
 app.use(cookieParser())
-mongoose.connect(key.mongoURI, {
-    useNewUrlParser : "true", useUnifiedTopology:"true", useCreateIndex:"true", useFindAndModify:"false"
-}).then(() => console.log("cunnect success"))
-  .catch(err => console.log(err))
-  
-app.get('/', (req, res) => res.send('안녕하세요! 연결되나요?'))
-app.get('/api/hello',(req, res)=>res.send("안녕하세요~"))
-app.post('/api/user/register', (req, res)=>{
-  const user = new User(req.body)
-  user.save((err,userInfo)=>{
-    if(err) return res.json({success:false, err})
-    return res.status(200).json({success:true})
-  })
-})
-app.post('/api/user/rpiregister', (req, res)=>{
-  const RPI = new rpi(req.body)
-  RPI.save((err, userInfo)=>{
-    if(err) return res.json({success:false, err})
-    return res.status(200).json({success:true})
-  })
+app.use(session({
+  secret: 'mykey',
+  saveUninitialized: true,
+  resave: true
+}));
 
+var connection = mysql.createConnection({
+  host: 'testdb.cv88vjxvcd70.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  port: 3306,
+  password: 'park0130',
+  database: 'testdb',
+  connectionLimit: 30
+});
+
+app.post('/api/user/register', (req, res)=>{
+  console.log(req.body);
+  var user_id = req.body.user_id;
+  var user_pw = req.body.user_pw;
+  var user_email = req.body.user_email;
+  var user_name = req.body.user_name;
+  var user_phone = req.body.user_phone;
+  var sql_query = "SELECT * FROM users where user_id=?";
+  connection.query(sql_query, user_id, function(err, rows){
+    if(rows.length ==0){
+      user_pw = cryptoM.encrypt(user_pw);
+      var register_query = "INSERT INTO users(user_id, user_pw, user_email, user_name, user_phone) values (?,?,?,?,?)";
+      var register_data=[user_id, user_pw, user_email, user_name, user_phone]
+      connection.query(register_query, register_data, function(err){
+        if(err){
+          console.log("user register fail - insert error")
+          res.status(400).send(err)
+        }else{
+          res.status(200).send('saved')
+        }
+      })
+    }else{
+      console.log("This user_id is already exist")
+      res.status(404).send(err)
+    }
+  })
 })
+
 
 
 app.post('/api/user/login', (req, res)=>{
-  User.findOne({email:req.body.email}, (err, userinfo)=>{
-    if(!userinfo){
-      return res.json({loginSuccess:false, message:"이메일이 존재하지 않습니다."})
-    }
-    userinfo.passwordCompare(req.body.password, (err, isMatch)=>{
-      if(!isMatch){
-        return res.json({loginSuccess:false, message:"비밀번호가 맞지 않습니다."})
+  var user_id=req.body.user_id;
+    var user_pw=req.body.user_pw;
+    var sqlquery = "SELECT  * FROM users WHERE user_id = ?";
+    connection.query(sqlquery, user_id,function (err, result) {
+            if (err) {
+                console.log("no match");
+            } else {
+                var bytes =cryptoM.decrypt(result[0].user_pw);
+                if(bytes===user_pw) {
+                    console.log("user login successfully");
+                    req.session.user_id=result[0].user_id;
+                    res.status(200).send({ user_id: result[0].user_id });
+                }else{
+                    console.log("wrong password!");
+                }
+            }
+        });
+})
+
+app.get('/api/user/logout', (req, res)=>{
+  if(req.session.user_id){
+    req.session.destroy(function(err){
+      if(err){
+        console.log("세선 삭제 실패")
+        return res.status(400).send(err)
       }
-      userinfo.generateToken((err, user)=>{
-        if(err) return res.status(400).send(err)
-        //토큰을 쿠키에 저장하기
-        res.cookie("x_auth", user.token).status(200).json({loginSuccess:true, userid:user._id})
-
-      })
+      console.log("로그아웃 성공")
+      return res.status(200).send({successfull : true})
     })
-  })
-})
-
-app.get('/api/user/auth', auth, (req, res)=>{
-  //여기까지 미들웨어를 통과하여 인증되었다면, authentication이 완료되었다는 말
-  res.status(200).json({
-    _id:req.user._id,
-    isAdmin:req.user.role === 0 ? false:true,
-    isAuth:true,
-    name: req.user.name,
-    email: req.user.email,
-    lastname: req.user.lastname,
-    role: req.user.role
-  })
-})
-app.get('/api/user/logout', auth, (req, res)=>{
-  User.findOneAndUpdate({_id:req.user._id}, {token:""}, (err, user)=>{
-    if(err) return res.json({success:false, err})
-    return res.status(200).send({success:true})
-  })
+  }else{
+    console.log("로그인 되어있지 않습니다.")
+    return res.status(400).send({successfull: false})
+  }
 })
 
 app.get('/api/user/rpilist', (req, res)=>{
-  rpi.find({email:req.body.email}, function(err, notice){
-    if(err) return res.json({success:false, err})
-    else{
-      if(notice.length==0){
-        return res.json({success:false, message:"there are no item"})
+  var sqlquery = 'SELECT * from rpilist where user_id =?'
+  connection.query(sqlquery, req.session.user_id, function(err, rows){
+    if(rows.length!=0){
+      if(err){
+        return res.status(400).send(err)
       }
-      else{
-        return res.json({success:true, result:notice})
-      }
+      console.log("search success")
+      return res.status(200).send(rows)
+    }else{
+      console.log("해당 유저의 RPI가 존재하지 않습니다.")
+      return res.status(200).send("RPI no exist")
     }
   })
 })
@@ -102,10 +123,26 @@ const dummyData={
 }
 app.get('/api/user/rpiinfo', (req, res)=>{
   var rpiList = req.body.list;
+  var result=[];
+  for(var i=0; i<=rpiList.length; i++){
+    result.push(Axios.get('https://endpoint/things/thingName/shadow?name=shadowName'))
+  }
   //list 에 있는 rpi 이름정보를 바탕으로 aws iot core의 shadow 정보를 모두 받아올것.
   //받아온 shoadow json 정보를 그대로 res.json(result)를 통해 반환할것
   // 이 구현이 끝나면 rpilist에 이 기능을 합치고 rpiinfo 부분은 그냥 삭제할것
-  return res.json(dummyData)
+  return res.status(200).send(result)
+})
+app.post('/api/user/rpiregister', (req, res)=>{
+
+
+})
+
+app.get('/api/user/auth', auth, (req, res)=>{
+  //여기까지 미들웨어를 통과하여 인증되었다면, authentication이 완료되었다는 말
+  res.status(200).json({
+    user_id:req.user._id,
+    isAuth:true,
+  })
 })
 //12강 복습할것!
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
